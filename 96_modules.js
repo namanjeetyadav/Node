17f4bf9b9f63e127bc1c8aa0a26e463c0c2a4a58
namanjeet <namanshuttler@gmail.com>
2024-09-21 23:31:04 +0530

    21.09.24

--- 96_modules.js
+++ 96_modules.js
@@ -1,1 +1,25 @@
-console.log("Hey I am starting node js")
+//Required package
+var pdf = require("pdf-node");
+var fs = require("fs");
+
+// Read HTML Template
+var html = fs.readFileSync("template.html", "utf8");
+var options = {
+    format: "A3",
+    orientation: "portrait",
+    border: "10mm",
+    header: {
+        height: "45mm",
+        contents: '<div style="text-align: center;">Author: Shyam Hajare</div>'
+    },
+    footer: {
+        height: "28mm",
+        contents: {
+            first: 'Cover page',
+            2: 'Second page', // Any page number is working. 1-based index
+            default: '<span style="color: #444;">{{page}}</span>/<span>{{pages}}</span>', // fallback value
+            last: 'Last Page'
+        }
+    }
+};
+
